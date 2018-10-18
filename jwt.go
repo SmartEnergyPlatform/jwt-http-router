2fca7308e21f1a7a1a903b76fb3eaef05e1b16a6
Ingo Rößner <roessner@infai.org>
2018-10-18 09:17:03 +0200

    init

---
+++ jwt.go
@@ -0,0 +1,230 @@
+/*
+ * Copyright 2018 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jwt_http_router
+
+import (
+	"net/http"
+
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strings"
+
+	"log"
+
+	"reflect"
+
+	"io"
+
+	"bytes"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type JwtConfig struct {
+	PubRsa    string
+	ForceAuth bool
+	ForceUser bool
+}
+
+type Jwt struct {
+	UserId         string                 `json:"sub"`
+	ResourceAccess map[string]Resource    `json:"resource_access"`
+	RealmAccess    Resource               `json:"realm_access"`
+	Map            map[string]interface{} `json:"-"`
+	Impersonate    JwtImpersonate         `json:"-"`
+}
+
+type JwtImpersonate string
+
+type Resource struct {
+	Roles []string `json:"roles"`
+}
+
+func (router *Router) jwt(r *http.Request) (token Jwt, err error) {
+	token.Map = map[string]interface{}{}
+	auth := r.Header.Get("Authorization")
+	if auth == "" {
+		if router.JwtConf.ForceAuth {
+			err = errors.New("missing Authorization header")
+		}
+	} else {
+		if router.JwtConf.PubRsa == "" {
+			err = GetJWTPayload(auth, &token.Map, &token)
+			if err != nil {
+				log.Println("error in GetJWTPayload() ", err)
+			}
+		} else {
+			err = GetJWTPayloadAndValidate(auth, router.JwtConf.PubRsa, &token.Map, &token)
+			if err != nil {
+				log.Println("error in GetJWTPayloadAndValidate() ", err)
+			}
+		}
+	}
+	if err == nil && router.JwtConf.ForceUser && token.UserId == "" {
+		err = errors.New("missing user id")
+	}
+	token.Impersonate = JwtImpersonate(auth)
+	return
+}
+
+func GetJWTPayload(auth string, results ...interface{}) (err error) {
+	authParts := strings.Split(auth, " ")
+	if len(authParts) != 2 {
+		return errors.New("expect auth string format like '<type> <token>'")
+	}
+	tokenString := authParts[1]
+	tokenParts := strings.Split(tokenString, ".")
+	if len(tokenParts) != 3 {
+		return errors.New("expect token string format like '<head>.<payload>.<sig>'")
+	}
+	payloadSegment := tokenParts[1]
+	err = DecodeJWTSegment(payloadSegment, results...)
+	return
+}
+
+// Decode JWT specific base64url encoding with padding stripped
+func DecodeJWTSegment(seg string, results ...interface{}) error {
+	if l := len(seg) % 4; l > 0 {
+		seg += strings.Repeat("=", 4-l)
+	}
+
+	b, err := base64.URLEncoding.DecodeString(seg)
+	if err != nil {
+		log.Println("error while base64.URLEncoding.DecodeString()", err, seg)
+		return err
+	}
+
+	for _, result := range results {
+		err = json.Unmarshal(b, result)
+		if err != nil {
+			log.Println("error while json.Unmarshal()", err, reflect.TypeOf(result).Kind().String(), string(b))
+			return err
+		}
+	}
+
+	return nil
+}
+
+func GetJWTPayloadAndValidate(auth string, pubRsaKey string, results ...interface{}) (err error) {
+	authParts := strings.Split(auth, " ")
+	if len(authParts) != 2 {
+		return errors.New("expect auth string format like '<type> <token>'")
+	}
+	tokenString := authParts[1]
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		//decode key base64 string to []byte
+		b, err := base64.StdEncoding.DecodeString(pubRsaKey)
+		if err != nil {
+			return nil, err
+		}
+		//parse []byte key to go struct key (use most common encoding)
+		return x509.ParsePKIXPublicKey(b)
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		b, err := json.Marshal(claims)
+		if err != nil {
+			return err
+		}
+		for _, result := range results {
+			err = json.Unmarshal(b, result)
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		err = errors.New("no valida JWT payload found")
+	}
+	return
+}
+
+func (this JwtImpersonate) Post(url string, contentType string, body io.Reader) (resp *http.Response, err error) {
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", string(this))
+	req.Header.Set("Content-Type", contentType)
+
+	resp, err = http.DefaultClient.Do(req)
+
+	if err == nil && resp.StatusCode == 401 {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(resp.Body)
+		resp.Body.Close()
+		log.Println(buf.String())
+		err = errors.New("access denied")
+	}
+	return
+}
+
+func (this JwtImpersonate) PostJSON(url string, body interface{}, result interface{}) (err error) {
+	b := new(bytes.Buffer)
+	err = json.NewEncoder(b).Encode(body)
+	if err != nil {
+		return
+	}
+	resp, err := this.Post(url, "application/json", b)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if result != nil {
+		err = json.NewDecoder(resp.Body).Decode(result)
+	}
+	return
+}
+
+func (this JwtImpersonate) Get(url string) (resp *http.Response, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", string(this))
+	resp, err = http.DefaultClient.Do(req)
+
+	if err == nil && resp.StatusCode == 401 {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(resp.Body)
+		resp.Body.Close()
+		log.Println(buf.String())
+		err = errors.New("access denied")
+	}
+	return
+}
+
+func (this JwtImpersonate) GetJSON(url string, result interface{}) (err error) {
+	resp, err := this.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return json.NewDecoder(resp.Body).Decode(result)
+}
---
+++ jwt_test.go
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2018 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package jwt_http_router
+
+import (
+	"fmt"
+	"testing"
+)
+
+const auth = `Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJoZDREMmR2dzVTYjNncmlZanR4eTdZaFQtTU1abm9WdDB3R2dqQnpad2U0In0.eyJqdGkiOiJiNGUxMWU0Mi0zNjQ3LTQ2ZWUtYTE2My0xOGVkZWRlOThjM2IiLCJleHAiOjE1MTg0NDUxMDEsIm5iZiI6MCwiaWF0IjoxNTE4NDQxNTAxLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwMDEvYXV0aC9yZWFsbXMvbWFzdGVyIiwiYXVkIjoiZnJvbnRlbmQiLCJzdWIiOiI2NTliNDZmNi1iZTFjLTRiNmYtODdjMy00ZTUxMWFhZTQ4MWMiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJmcm9udGVuZCIsIm5vbmNlIjoiZjVkYmFlMzktYjhkMC00ZjUxLTg2ZTMtNGViZmUzMDA4NWI3IiwiYXV0aF90aW1lIjoxNTE4NDQxNDk5LCJzZXNzaW9uX3N0YXRlIjoiYWFhN2Q0ZTAtNDJjNi00Y2FkLTkwMjUtYWY5NjcwMTFmM2QyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyJodHRwOi8vbG9jYWxob3N0OjUwMDAiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImNyZWF0ZS1yZWFsbSIsImFkbWluIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJtYXN0ZXItcmVhbG0iOnsicm9sZXMiOlsidmlldy1pZGVudGl0eS1wcm92aWRlcnMiLCJ2aWV3LXJlYWxtIiwibWFuYWdlLWlkZW50aXR5LXByb3ZpZGVycyIsImltcGVyc29uYXRpb24iLCJjcmVhdGUtY2xpZW50IiwibWFuYWdlLXVzZXJzIiwicXVlcnktcmVhbG1zIiwidmlldy1hdXRob3JpemF0aW9uIiwicXVlcnktY2xpZW50cyIsInF1ZXJ5LXVzZXJzIiwibWFuYWdlLWV2ZW50cyIsIm1hbmFnZS1yZWFsbSIsInZpZXctZXZlbnRzIiwidmlldy11c2VycyIsInZpZXctY2xpZW50cyIsIm1hbmFnZS1hdXRob3JpemF0aW9uIiwibWFuYWdlLWNsaWVudHMiLCJxdWVyeS1ncm91cHMiXX0sImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInByZWZlcnJlZF91c2VybmFtZSI6InNlcGwifQ.cVHbNDhAmg9_r00TTsXMWjh8KvsyGlqezCl_9M6i3iJghWu0dB6xcUhCNE7IUSXIsFl7aifRooTNgUJI8yUZWVVJkvVySBhOXf1y7p1-yxOCVBmfyhqFYiqbJRh6-DKXNbHZIxsJGSbcf3Z98rG92MTiE51Y6pVvWZW9TIkjSIMU-rnvAQ5UUssPd6T2q1wz0VUNGdbLS1-7JnKjqU7XzEo3xbJdHt_v9NmdKqknvpCxVbYIgCphypM06PZTAKWBUGu_dNRIQ5XoAAIjRy09XTfD9vKQudIk-xpIc6GVdGl1JILeRoLPriVrKPAO_-dotNI2hk_NBUei5rMbZ8dC1A`
+
+func TestJwt(t *testing.T) {
+	token := Jwt{}
+	token.Map = map[string]interface{}{}
+	err := GetJWTPayload(auth, &token.Map, &token)
+	fmt.Println(err, token)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestRsaJwt(t *testing.T) {
+	token := Jwt{}
+	token.Map = map[string]interface{}{}
+	rsa := "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApIH2UK4yoHkQQyTVWKvcbtNrfa9t7UBFdgLTYCZ47qjVXLGa6XJG+LDEeMjOZqA7irSfuBayV9CX6rJApxibvfF5UnCbV7hCcpu5cJd2ezWWkcvj08ZpLCbYwI6OQzXIAYwZYSwQQW1VnAfqNryO2Mb8g2f++1C+2PuX0DcpKt8Wz1sjjdoNkbDPHL3JtGUhnLHSCh/Qz0crqIQBwymCj/qKaRaQ8U9VX3xasjGalnOA7z2503KaWlpZV5N6h9QH/FqQ3rVyw/4cnOmwYH705Lqyb0RVan90/vJKbVh8YBbW7JGbRrtbW6jfg9YJSsPBPT8eUV90O9aYQNxB+W/30wIDAQAB"
+	err := GetJWTPayloadAndValidate(auth, rsa, &token.Map, &token)
+	fmt.Println(err, token)
+	if err != nil {
+		t.Error(err)
+	}
+}
